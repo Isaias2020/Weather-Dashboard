2ba874c6189a7384e1bcc9cfb0b0a3404dcd032a
Isaias Quintanilla <quintanillaisaias@gmail.com>
2020-05-08 15:48:26 -0500

    changed layout of dashboard

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-// Time and date through Moment.js
+// Time and date through Moment.js for current day
 $(document).ready(function () {
     var currentDay = $("#day-of-week");
     var todaysDate = moment().format('dddd,MMMM Do YYYY');
     currentDay.text(todaysDate);
 });
+
+var APIKey = "&appid=b397225e3e6e7043cc26284176c744bd";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+var citiesArray = [];
